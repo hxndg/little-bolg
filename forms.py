ee3e4aa22fec1118dab3d726008d333ad16c35d6
sumy7 <sunmingjian8@gmail.com>
2016-11-17 18:18:34 +0800

    rewrite addArticle and article page-style

--- forms.py
+++ forms.py
@@ -1,11 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextField,IntegerField,PasswordField
+from wtforms import StringField,TextField,IntegerField,PasswordField, TextAreaField
 from wtforms.validators import DataRequired
 
 class ArticleForm(FlaskForm):
     title = StringField('title',validators = [DataRequired()])
-    content = TextField('content',validators = [DataRequired()])
-
+    content = TextAreaField('content',validators = [DataRequired()])
 
 class UserForm(FlaskForm):
     username = StringField('username',validators = [DataRequired()])
@@ -18,4 +17,4 @@ class SignUpForm(FlaskForm):
     email = StringField('email',validators=[DataRequired()])
 
 class ReplyForm(FlaskForm):
-    content = TextField('content',validators = [DataRequired()])
+    content = TextAreaField('content',validators = [DataRequired()])
